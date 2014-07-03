a61cbbb967b43616cd99576f4cbadf096a1a3b29
zziceoilator <edgarcia91@hotmail.com>
2014-07-03 14:14:07 +0200

    este es la 4 con la correccion de errores

--- StockLlantas/src/CalculoAlmacen.java
+++ StockLlantas/src/CalculoAlmacen.java
@@ -16,9 +16,9 @@ public class CalculoAlmacen {
 	
 	public int calcularTotalAlmacen1(){
 		int total=0;
-		for(int i=0;i<llantasAlmacen2.length;i++)
+		for(int i=0;i<llantasAlmacen1.length;i++)
 		{
-			total=llantasAlmacen2[i].numeroLlantas+total;
+			total=llantasAlmacen1[i].numeroLlantas+total;
 		}
 		return total;
 	}
@@ -37,7 +37,7 @@ public class CalculoAlmacen {
 	}
 	
 	public void anyadirLlantaAlmacen2(int posicion){
-		llantasAlmacen1[posicion].addLlanta();
+		llantasAlmacen2[posicion].addLlanta();
 	}
 	
 }
